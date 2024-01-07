7ac533a9c8ad7e26f921f3bc0c2cf71ea93063ff
Arlene <138469304+gowbi@users.noreply.github.com>
2024-01-07 18:32:52 +0800

    new update

---
+++ src/server/api/routers/topic.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+
+export const topicRouter = createTRPCRouter({
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.topic.findMany({
+      where: { 
+        userId: ctx.session.user.id,
+      }
+    });
+  }),
+
+  create: protectedProcedure
+  .input(z.object({ title: z.string() }))
+    .mutation(async ({ ctx, input }) => { 
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      return ctx.db.topic.create({
+        data: {
+          title: input.title,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+});
